8ef4be1a81b9b330447aa14a09976f90e9b8bc19
Muayyad Alsadi مؤيد السعدي <alsadi@ojuba.org>
2010-06-12 22:15:49 +0300

    add Makefile and webAppTest.py

---
+++ Makefile
@@ -0,0 +1,35 @@
+DESTDIR?=/
+datadir?=$(DESTDIR)/usr/share
+INSTALL=install
+
+SOURCES=$(wildcard *.desktop.in)
+TARGETS=${SOURCES:.in=}
+
+all: $(TARGETS) icons
+
+icons:
+	for i in 96 72 64 48 36 32 24 22 16; do \
+		convert -background none thawab.svg -resize $${i}x$${i} thawab-$${i}.png; \
+	done
+pos:
+	make -C po all
+
+install: all
+	python setup.py install -O2 --root $(DESTDIR)
+	$(INSTALL) -d $(datadir)/applications/
+	$(INSTALL) -m 0644 thawab.desktop $(datadir)/applications/
+	$(INSTALL) -m 0644 -D thawab.svg $(datadir)/icons/hicolor/scalable/apps/thawab.svg;
+	for i in 96 72 64 48 36 32 24 22 16; do \
+		install -d $(datadir)/icons/hicolor/$${i}x$${i}/apps; \
+		$(INSTALL) -m 0644 -D thawab-$${i}.png $(datadir)/icons/hicolor/$${i}x$${i}/apps/thawab.png; \
+	done
+
+%.desktop: %.desktop.in pos
+	intltool-merge -d po $< $@
+
+clean:
+	rm -f $(TARGETS)
+	for i in 96 72 64 48 36 32 24 22 16; do \
+		rm -f thawab-$${i}.png; \
+	done
+
---
+++ webAppTest.py
@@ -0,0 +1,26 @@
+#! /usr/bin/python
+# -*- coding: UTF-8 -*-
+import sys, os, os.path, logging
+import Thawab.core
+from Thawab.webApp import webApp
+
+prefix=os.path.dirname(sys.argv[0])
+th=Thawab.core.ThawabMan(os.path.expanduser('~/.thawab'), isMonolithic=False)
+
+myLogger=logging.getLogger('ThawabWebAppTest')
+h=logging.StreamHandler() # in production use WatchedFileHandler or RotatingFileHandler
+h.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
+myLogger.addHandler(h)
+myLogger.setLevel(logging.INFO)
+
+from paste import httpserver
+app=webApp(
+  th,True, 
+  os.path.join(prefix,'thawab-files','templates'),
+  staticBaseDir={'/_files/':os.path.join(prefix,'thawab-files','media')},
+  logger=myLogger
+  );
+# for options see http://pythonpaste.org/modules/httpserver.html
+httpserver.serve(app, host='0.0.0.0', port='8080') # to serve publically
+#httpserver.serve(app, host='127.0.0.1', port='8080') # to serve localhost
+
