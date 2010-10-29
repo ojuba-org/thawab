bc9e1a5237c4f8b8423f895496acdc85970254ed
Muayyad Alsadi مؤيد السعدي <alsadi@ojuba.org>
2010-10-29 10:05:11 +0200

    proper async indexer

--- Thawab/asyncIndex.py
+++ Thawab/asyncIndex.py
@@ -95,8 +95,8 @@ class AsyncIndex():
         method, args, kw=item
         f=getattr(self.searchEngine, method)
         f(*args,**kw)
-        self._q.task_done()
         if self._q.qsize()==0: self.searchEngine.indexingEnd()
+        self._q.task_done()
         self.lock.acquire(); self.running-=1; self.lock.release()
       elif self._q.empty():
         if self.end_when_done: self.keepworking=False
